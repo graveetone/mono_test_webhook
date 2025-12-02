43744e3de3d5b25d91f9fa097fe5c1254fe006d2
Sviat Zastavskyi <98279909+graveetone@users.noreply.github.com>
2025-12-02 17:25:02 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.post("/webhook")
+async def post_test_webhook(request: Request):
+    body = await request.json()
+    return {"status": "ok", "body: body}
+
+@app.get("/webhook")
+async def get_test_webhook(request: Request):
+    print(f"Webhook received aaaaaaaaaaaaaaaaaaaaaa")
