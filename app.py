d4d1ffd85df7ca050c0494e18a9330c38e0a81d8
Sviat Zastavskyi <98279909+graveetone@users.noreply.github.com>
2025-12-02 17:45:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = FastAPI()
 @app.post("/webhook")
 async def post_test_webhook(body: dict):
     # body = await request.json()
+    print(body)
     return {"status": "ok", "body": body}
 
 @app.get("/webhook")
